f66e6204e5d063b4132136cfee6ac5dec4def394
bhalios <babisel10@gmail.com>
2011-09-29 01:13:18 +0300

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include "processor.h"
+#include "mipsPipelined.h"
+#include "register_file.h"
+#include "memory.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	mipsPipelined *proc;
+	Register_File *regs = new Register_File();
+	simpleMemory *mem = new simpleMemory( 4096 );
+
+	mem->storeWord( 100, 0x2003000a );		// addi $3,$0,10 
+	mem->storeWord( 104, 0xac030004 );		// sw $3,4($0)
+	mem->storeWord( 108, 0x00630821 );		// add $1,$3,$3
+	mem->storeWord( 112, 0xac010008 );		// sw $1,8($0)
+
+	proc = new mipsPipelined( mem, regs, 100, 112 );
+
+	cout << "Processor constructed..." << endl;
+
+	while( true ) {
+		proc->step();
+		proc->printRegisters();
+		if( getchar() == EOF )
+			break;	
+	}
+
+	return 0;
+}
